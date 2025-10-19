22934d8711a36974a37b7571f79feec080683cb8
Mayel <smayel.dev@gmail.com>
2025-10-19 02:18:20 -0500

    config: ajustes globales del proyecto (rutas, estilos, Tailwind y configuración base)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { CotizacionPageComponent } from './features/cotizaciones/pages/cotizacion-page/cotizacion-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'cotizacion',
+    component: CotizacionPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'cotizacion',
+  },
+];
